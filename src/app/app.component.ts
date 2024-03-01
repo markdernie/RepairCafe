7be090ab34f838a083f3df0612724ba01c30e33b
Mark Dernie <mark.dernie@gmail.com>
2024-03-01 15:39:37 +0000

    logo toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule} from '@angular/material/toolbar'
 import { RepairComponent } from './repair/repair/repair.component'
+import { HeaderComponent} from './common/header/header.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, RepairComponent],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, HeaderComponent,RepairComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
