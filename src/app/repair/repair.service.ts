d90aafc2828e28b9794271ebc69caaf086187022
Mark Dernie <mark.dernie@red-opal.com>
2024-03-07 09:21:31 +0000

    before tanstack

--- src/app/repair/repair.service.ts
+++ src/app/repair/repair.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Repair} from '../repair/repair'
-//import {} from '@a'
+import { Repair } from '../repair/repair'
+import { HttpClient } from '@angular/common/http'
 import { Form, FormBuilder} from '@angular/forms'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepairService {
+
+  url:string ='http://KateLaptop:3000'
   
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRepair() {
+    return this.http.get(`http://KateLaptop:3000`)
+  }
+  putRepair(repair:Repair) {
+
+    let junk=this.http.put(this.url,repair)
+    .subscribe((data)=>{console.log('Here')})
+    console.log('junk',junk)
+  }
+    
+    //return this.http.put(this.url,repair)
+  
 }
--- src/app/repair/repair/repair.component.ts
+++ src/app/repair/repair/repair.component.ts
@@ -1,27 +1,66 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormBuilder} from '@angular/forms'
-import {Repair} from '../repair'
-import {Observable,interval,map,of} from 'rxjs'
-
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms'
+import { FormGroup, FormControl } from '@angular/forms';
+import { Repair } from '../repair'
+import { Observable, interval, map, of } from 'rxjs'
+import { RepairService } from '../repair.service';
+import { HttpClient } from '@angular/common/http'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-repair',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatCardModule, MatButtonModule],
   templateUrl: './repair.component.html',
-  styleUrl: './repair.component.scss'
+  styleUrl: './repair.component.scss',
+  providers: [HttpClient]
 })
 export class RepairComponent {
 
 
-//currentTime$:Observable<Date>
+  //currentTime$:Observable<Date>
 
-obs: { name: string }[] = [];
-obs$ = of([{ name: 'Louie' }, { name: 'Tess' }])
+  obs: { name: string }[] = [];
+  obs$ = of([{ name: 'Louie' }, { name: 'Tess' }])
+
+  repairlist: Repair[] = [];
+  repair: Repair= new Repair()
+  repair$ = of([{ id: '1' }])
+  //myData$:Observable<any>= of(new Repair)
+  myData$: Observable<any> = of({})
+  repairform = this.formsbuilder.group({
+    // id: ['',Validators.required],
+    // age: 0,
+    description: ['',Validators.required]
+  })
   //.subscribe((result: any) => this.obs = result);
+  constructor(private repairService: RepairService, private formsbuilder: FormBuilder) { 
+    console.log('newrepair',this.repair)
+  }
 
-ngOnInit() {
-  //this.currentTime$=interval(1000).pipe(map(()=> new Date()))
-}
+  ngOnInit() {
+    console.log('running ngOnInit')
+    this.myData$ = this.repairService.getRepair()
+    console.log("myData:", this.myData$)
+    //this.currentTime$=interval(1000).pipe(map(()=> new Date()))
+  }
+
+  save(repairform: FormGroup) {
+    // console.log('Click Save', repairform.value)
+    // console.log('New repair:',this.newrepair)
+    this.repair.description=repairform.value.description
+    console.log('put repair:',this.repair)
+    this.repairService.putRepair(this.repair)
+  }
+
+  
+
+  // new (){
+  //   repair=new Repair
+  // }
+
 }
